d1f0632eb54c8940616475da6fdcd3b8bdcd70fe
Ben Simms <ben@bensimms.moe>
2020-04-22 22:44:43 +0100

    Remove badness, cleanup

--- src/DiPolysemy.hs
+++ src/DiPolysemy.hs
@@ -24,12 +24,7 @@ module DiPolysemy
     , critical_
     , emergency_ ) where
 
-import           Control.Concurrent         ( forkIOWithUnmask, myThreadId )
-import qualified Control.Concurrent.STM     as STM
-import qualified Control.Exception          as Ex ( BlockedIndefinitelyOnSTM
-                                                  , asyncExceptionFromException
-                                                  , asyncExceptionToException )
-import qualified Control.Exception.Safe     as Ex
+import           Data.Functor
 
 import qualified Df1                        as D
 
@@ -37,14 +32,9 @@ import qualified Di.Core                    as DC
 import qualified Di.Df1                     as Df1
 import qualified Di.Handle                  as DH
 
-import           Language.Haskell.TH.Syntax
-
 import           Polysemy
-import           Polysemy.Resource
 
 import           Prelude                    hiding ( error, log )
-
-import qualified Unsafe.TrueName            as TN
 
 data Di level path msg m a where
   Log    :: level -> msg -> Di level path msg m ()
@@ -54,101 +44,48 @@ data Di level path msg m a where
 
 makeSem ''Di
 
--- aaagh
-mkDi_internal = $(fmap ConE $ TN.summon "Di" =<< TN.summon "Di" 'DC.new)
+data DiIOInner m a where
+  RunDiIOInner :: (DC.Log level D.Path msg -> IO ()) -> (DC.Di level D.Path msg -> m a) -> DiIOInner m a
 
-data MyThreadKilled = MyThreadKilled
-  deriving ( Show )
+makeSem ''DiIOInner
 
-instance Ex.Exception MyThreadKilled where
-  toException = Ex.asyncExceptionToException
+diToIO :: forall r a. Member (Embed IO) r => Sem (DiIOInner ': r) a -> Sem r a
+diToIO = interpretH
+  (\case RunDiIOInner commit a -> do
+           istate <- getInitialStateT
+           ma <- bindT a
 
-  fromException = Ex.asyncExceptionFromException
+           withLowerToIO $ \lower finish -> do
+             let done :: Sem (DiIOInner ': r) x -> IO x
+                 done = lower . raise . diToIO
 
-runDiToIO :: forall r level msg a.
-          Members '[Embed IO] r
-          => (DC.Log level D.Path msg -> IO ())
-          -> Sem (Di level D.Path msg ': r) a
-          -> Sem r a
-runDiToIO commit m = resourceToIO $ do
-  tqLogs <- embed $ STM.newTQueueIO
-  tmvWOut <- embed $ STM.newEmptyTMVarIO
-  let di = mkDi_internal (\_ _ _ -> True)
-        (\x -> STM.tryReadTMVar tmvWOut >>= \case
-           Nothing -> STM.writeTQueue tqLogs x
-           Just ex -> STM.throwSTM ex)
-        (STM.tryReadTMVar tmvWOut >>= \case
-           Nothing -> STM.isEmptyTQueue tqLogs >>= \case
-             True  -> pure ()
-             False -> STM.retry
-           Just ex -> STM.throwSTM ex) (\_ _ -> Just (pure ()))
-  tIdMe <- embed myThreadId
-  tIdWorker <- embed $ forkIOWithUnmask $ \unmask -> do
-    Ex.tryAsync (unmask (worker tqLogs)) >>= \case
-      Right ()                      -> STM.atomically $ do
-        STM.putTMVar tmvWOut (Ex.toException DC.LoggingWorkerNotRunning)
-      Left (se :: Ex.SomeException) -> case Ex.fromException se of
-        Just MyThreadKilled -> STM.atomically $ do
-          STM.putTMVar tmvWOut (Ex.toException DC.LoggingWorkerNotRunning)
-        Nothing             -> do
-          let ex = DC.ExceptionInLoggingWorker se
-          Ex.finally (Ex.throwTo tIdMe (Ex.asyncExceptionToException ex))
-            (STM.atomically (STM.putTMVar tmvWOut (Ex.toException ex)))
-  finally (raise $ go di m)
-    (embed $ Ex.finally (DC.flush di) (Ex.throwTo tIdWorker MyThreadKilled))
+             DC.new commit (\di -> do
+                               res <- done (ma $ istate $> di)
+                               finish
+                               pure res))
+
+runDiToIO
+  :: forall r level msg a.
+  Member (Embed IO) r
+  => (DC.Log level D.Path msg -> IO ())
+  -> Sem (Di level D.Path msg ': r) a
+  -> Sem r a
+runDiToIO commit m = diToIO $ runDiIOInner commit (`go` raiseUnder m)
   where
-    worker q = do
-      Ex.try (STM.atomically $ STM.peekTQueue q) >>= \case
-        Left (_ :: Ex.BlockedIndefinitelyOnSTM) -> pure ()
-        Right log_                              -> do
-          _ <- Ex.tryAny (commit log_)
-          _ <- STM.atomically (STM.readTQueue q)
-          worker q
-
-    go :: Member (Embed IO) r0
-       => DC.Di level D.Path msg
-       -> Sem (Di level D.Path msg ': r0) a0
-       -> Sem r0 a0
-    go di m' = (`interpretH` m') $ \case
+    go :: Member (Embed IO) r0 => DC.Di level D.Path msg -> Sem (Di level D.Path msg ': r0) a0 -> Sem r0 a0
+    go di m = (`interpretH` m) $ \case
       Log level msg -> do
         t <- embed $ DC.log di level msg
         pureT t
       Flush         -> do
         t <- embed $ DC.flush di
         pureT t
-      Push s m''    -> do
-        mm <- runT m''
+      Push s m'     -> do
+        mm <- runT m'
         raise $ go (Df1.push s di) mm
-      Attr_ k v m'' -> do
-        mm <- runT m''
+      Attr_ k v m'  -> do
+        mm <- runT m'
         raise $ go (Df1.attr_ k v di) mm
-
--- when there's some way of having `MonadMask (Sem r)` work, we can use the below
--- instead of doing the above nasty
-
--- runDiToIO
---   :: forall r level msg a.
---   (Ex.MonadMask (Sem r), Member (Embed IO) r)
---   => (DC.Log level D.Path msg -> IO ())
---   -> Sem (Di level D.Path msg ': r) a
---   -> Sem r a
--- runDiToIO commit m = do
---   DC.new commit $ (`go` m)
---   where
---     go :: DC.Di level D.Path msg -> Sem (Di level D.Path msg ': r) a0 -> Sem r a0
---     go di m = (`interpretH` m) $ \case
---       Log level msg -> do
---         t <- embed $ DC.log di level msg
---         pureT t
---       Flush         -> do
---         t <- embed $ DC.flush di
---         pureT t
---       Push s m'     -> do
---         mm <- runT m'
---         raise $ go (Df1.push s di) mm
---       Attr_ k v m'  -> do
---         mm <- runT m'
---         raise $ go (Df1.attr_ k v di) mm
 
 runDiToStderrIO :: Member (Embed IO) r => Sem (Di D.Level D.Path D.Message ': r) a -> Sem r a
 runDiToStderrIO m = do
